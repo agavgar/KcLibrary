ece996e1121cc665406a004b9d904ce4e9510565
Alejandro Alberto Gavira García <alex.gavira@gmail.com>
2024-03-12 19:41:07 +0100

    First

--- Package.swift
+++ Package.swift
@@ -11,13 +11,23 @@ let package = Package(
             name: "KcLibrary",
             targets: ["KcLibrary"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "20.0.0"))
+    
+    
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KcLibrary"),
+            name: "KcLibrary",
+        dependencies: [
+            .product(name: "KeychainSwift", package: "keychain-swift")
+        
+        ]
+        ),
         .testTarget(
             name: "KcLibraryTests",
-            dependencies: ["KcLibrary"]),
-    ]
+            dependencies: ["KcLibrary"]
+        ),]
 )
--- Sources/KcLibrary/Keychain.swift
+++ Sources/KcLibrary/Keychain.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
-//  
+//  Keychain.swift
+//
 //
 //  Created by Alejandro Alberto Gavira García on 12/3/24.
 //
 
 import Foundation
+import KeychainSwift
+
+public struct KeychainKC {
+    
+    public init(){}
+    
+    //funcion guardar
+    @discardableResult
+    public func saveKC(key: String, value: String) -> Bool {
+        if let data = value.data(using: .utf8){
+            return KeychainSwift().set(data, forKey: key)
+        }else{
+            return false
+        }
+    }
+    
+    public func loadKC(key:String) -> String? {
+        if let data = KeychainSwift().get(key){
+            return data
+        }else{
+            return nil
+        }
+    }
+    
+    @discardableResult
+    public func deleteKC(key: String) -> Bool {
+        KeychainSwift().delete(key)
+    }
+    
+}
